52a73ec2d1615ea877ca3a964156b6f7022d9f85
Darius Kazemi <darius.kazemi@gmail.com>
2013-02-04 11:20:20 -0500

    Updated so that commands don't clobber each other and promises work correctly

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 var APIKEY = 'd4711e36506fe047f12970c6fdc0b43f6e4fef2e855e03d5c';
 var Wordnik = require('./lib/wordnik-bb.js').init(APIKEY);
+var $ = require('jquery');
 
 var word = new Wordnik.Word({word: 'king', params:{includeSuggestions:true}});
- 
-word.getWord().then(function(word) {
-  word.getExamples().then(function(word) {
-    word.getDefinitions().then(function(word) {
-      word.getTopExample().then(function(word) {
-        word.getRelatedWords().then(function(word) {
-          word.getPronunciations().then(function(word) {
-            word.getScrabbleScore().then(function(word) {
-              word.getHyphenation().then(function(word) {
-                word.getFrequency().then(function(word) {
-                  word.getPhrases().then(function(word) {
-                    word.getEtymologies().then(function(word) {
-                      word.getAudio().then(function(word) {
-                        console.log(word.get("audio"));
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  }); 
-});
+// Make it so that the get functions return promises!
+$.when( 
+    word.getExamples(),
+    word.getDefinitions(),
+    word.getTopExample(),
+    word.getRelatedWords(),
+    word.getPronunciations(),
+    word.getScrabbleScore(),
+    word.getHyphenation(),
+    word.getFrequency(),
+    word.getPhrases(),
+    word.getEtymologies(),
+    word.getAudio()
+  )
+  .then(function() {
+    console.log(word);
+  })
+  .fail(function() {
+    console.log("failed!");
+  });
