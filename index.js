09333e1c45436bf27b604dd5a7d828b4a444b102
Darius Kazemi <darius.kazemi@gmail.com>
2013-02-03 20:04:25 -0500

    Updating to provide multiple methods on the Word model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var APIKEY = 'd4711e36506fe047f12970c6fdc0b43f6e4fef2e855e03d5c';
 var Wordnik = require('./lib/wordnik-bb.js').init(APIKEY);
 
-var word = new Wordnik.Word({word: 'kings', includeSuggestions: true});
+var word = new Wordnik.Word({word: 'kings', params:{includeSuggestions:true}});
  
 word.getWord();
