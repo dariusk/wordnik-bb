a29dedaf59c64447ebb953b55198edef76161a98
Darius Kazemi <darius.kazemi@gmail.com>
2013-02-04 10:10:51 -0500

    Added the rest of the base word API calls. TODO: replace naive implementations, parse the responses to that anything that returns an array is encapsulated in an object because god damn this could use some consistent responses.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var APIKEY = 'd4711e36506fe047f12970c6fdc0b43f6e4fef2e855e03d5c';
 var Wordnik = require('./lib/wordnik-bb.js').init(APIKEY);
 
-var word = new Wordnik.Word({word: 'kings', params:{includeSuggestions:true}});
+var word = new Wordnik.Word({word: 'king', params:{includeSuggestions:true}});
  
 word.getWord().then(function(word) {
   word.getExamples().then(function(word) {
@@ -10,7 +10,17 @@ word.getWord().then(function(word) {
         word.getRelatedWords().then(function(word) {
           word.getPronunciations().then(function(word) {
             word.getScrabbleScore().then(function(word) {
-              console.log(word);
+              word.getHyphenation().then(function(word) {
+                word.getFrequency().then(function(word) {
+                  word.getPhrases().then(function(word) {
+                    word.getEtymologies().then(function(word) {
+                      word.getAudio().then(function(word) {
+                        console.log(word.get("audio"));
+                      });
+                    });
+                  });
+                });
+              });
             });
           });
         });
