7f5ebd1bb3a647a3d3fefd0c88cfc010b527f455
Darius Kazemi <darius.kazemi@gmail.com>
2013-02-05 09:23:38 -0500

    Updated the way random words work. Now you can request a promise for a random word in a Backbone model.

--- index.js
+++ index.js
@@ -2,23 +2,19 @@ var APIKEY = 'd4711e36506fe047f12970c6fdc0b43f6e4fef2e855e03d5c';
 var Wordnik = require('./lib/wordnik-bb.js').init(APIKEY);
 var $ = require('jquery');
 
-//var word = new Wordnik.Word({word: 'king', params:{includeSuggestions:true}});
-
-/*
+// Here's an example of declaring a new word and then populating it with the data from every available word method.
+var word = new Wordnik.Word({word: 'king', params:{includeSuggestions:true}});
 word.getEverything()
-  .then( function() {
-    console.log(word);
+ .then( function() {
+    console.log("A WHOLE lot of data in a Word model: ", word);
   });
-*/
 
-var random = new Wordnik.RandomWord({
-  params: {
+// Here's an example of using the getRandomWordModel function, which behind the scenes generates a random word and then creates a Wordnik.Word model based on it.
+var randomWordPromise = Wordnik.getRandomWordModel({
     includePartOfSpeech: "verb-transitive",
     minCorpusCount: 10000
   }
+);
+randomWordPromise.done(function(word) {
+  console.log("The model for our random word: ", word);
 });
-
-random.getRandomWord()
-  .then( function() {
-    console.log("Here's a random word: " + random.get("word"));
-  });
